2ffeaef4ec3315a2ab8dd6db1d7df3e511d18ede
sanathkkss12@gmail.com <sanathkkss12@gmail.com>
2026-01-25 10:02:11 +0530

    Syncing Changes

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,34 @@
+const api_key = "8f9b712b72f1de1f2c468144831b7932";
+
 /**
- * @license MIT
- * @fileoverview All api related stuff like api_key, api request etc.
- * @copyright codewithsadee 2023 All rights reserved
- * @author codewithsadee <mohammadsadee24@gmail.com>
- */ ///
+ *Fetch Data From Server
+ *@param {string} URL - API URL
+ *@param {Function} callback callback
+ */
+export const fetchData = function (URL, callback) {
+  fetch(`${URL}&appid=${api_key}`)
+    .then((res) => res.json())
+    .then((data) => callback(data));
+};
+
+export const url = {
+  currentWeather(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`;
+  },
+
+  forecast(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`;
+  },
+
+  airPollution(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`;
+  },
+
+  reverseGeo(lat, lon) {
+    return `https://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`;
+  },
+
+  geo(query) {
+    return `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`;
+  },
+};
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,497 @@
+"use strict";
+
+import { fetchData, url } from "./api.js";
+import * as module from "./module.js";
+
 /**
- * @license MIT
- * @copyright codewithsadee 2023 All rights reserved
- * @author codewithsadee <mohammadsadee24@gmail.com>
+ * Add event listener on vatious elements
+ * @param {NodeList} elements Element node array
+ * @param {string} eventType Event Type e.g.: "click", "mouseover"
+ * @param {function} callback Callback function
  */
+const addEventOnElements = function (elements, eventType, callback) {
+  for (const element of elements) element.addEventListener(eventType, callback);
+};
+
+/**
+ * Toggle search in mobile device
+ *
+ */
+
+const searchView = document.querySelector("[data-search-view]");
+const searchTogglers = document.querySelectorAll("[data-search-toggler]");
+
+const toggleSearch = () => searchView.classList.toggle("active");
+addEventOnElements(searchTogglers, "click", toggleSearch);
+
+//SEARCH INTEGRATION
+
+const searchField = document.querySelector("[data-search-field]");
+const searchResult = document.querySelector("[data-search-result]");
+
+let searchTimeout = null;
+const searchTimeoutDuration = 500;
+
+searchField.addEventListener("input", function () {
+  searchTimeout ?? clearTimeout(searchTimeout);
+  // if (searchTimeout) clearTimeout(searchTimeout);
+
+  if (!searchField.value) {
+    searchResult.classList.remove("active");
+    searchResult.innerHTML = "";
+    searchResult.classList.remove("searching");
+  } else {
+    searchField.classList.add("searching");
+  }
+
+  if (searchField.value) {
+    searchTimeout = setTimeout(() => {
+      fetchData(url.geo(searchField.value), function (locations) {
+        searchField.classList.remove("searching");
+        searchResult.classList.add("active");
+        searchResult.innerHTML = `
+          <ul class="view-list" data-search-list></ul>
+        `;
+
+        const items = [];
+
+        for (const { name, lat, lon, country, state } of locations) {
+          const searchItem = document.createElement("li"); //////
+          searchItem.classList.add("view-item");
+
+          searchItem.innerHTML = `
+            <span class="m-icon">location_on</span>
+            <div>
+              <p class="item-title">${name}</p>
+              <p class="label-2 item-subtitle">${state || ""} ${country}</p>
+            </div>
+
+            <a href="#/weather?lat=${lat}&lon=${lon}" class="item-link has-state" aria-label="${name} weather" data-search-toggler></a>
+          `;
+
+          searchResult
+            .querySelector("[data-search-list]")
+            .appendChild(searchItem);
+
+          items.push(searchItem.querySelector("[data-search-toggler]"));
+        }
+
+        addEventOnElements(items, "click", function () {
+          toggleSearch();
+          searchResult.classList.remove("active");
+        });
+      });
+    }, searchTimeoutDuration);
+  }
+});
+
+const container = document.querySelector("[data-container]");
+const loading = document.querySelector("[data-loading]");
+const currentLocationBtn = document.querySelector(
+  "[data-current-location-btn]",
+);
+const errorContent = document.querySelector("[data-error-content]");
+
+/**
+ * Render Weather data in HTML
+ * @param {number} lat Latitude
+ * @param {number} lon Longitude
+ */
+export const updateWeather = function (lat, lon) {
+  loading.style.display = "grid";
+  // container.style.overflowY = "hidden";
+  container.classList.remove("fade-in");
+  errorContent.style.display = "none";
+
+  const currentWeatherSection = document.querySelector(
+    "[data-current-weather]",
+  );
+  const highlightSection = document.querySelector("[data-highlights]");
+  const hourlySection = document.querySelector("[data-hourly-forecast]");
+  const forecastSection = document.querySelector("[data-5-day-forecast]");
+
+  currentWeatherSection.innerHTML = ``;
+  highlightSection.innerHTML = ``;
+  hourlySection.innerHTML = ``;
+  forecastSection.innerHTML = ``;
+
+  if (window.location.hash === "#/current-location") {
+    currentLocationBtn.setAttribute("disabled", "");
+  } else {
+    currentLocationBtn.removeAttribute("disabled");
+  }
+
+  // CURRENT WEATHER SECTION
+
+  fetchData(url.currentWeather(lat, lon), function (currentWeather) {
+    const {
+      weather,
+      dt: dateUnix,
+      sys: { sunrise: sunriseUnixUTC, sunset: sunsetUnixUTC },
+      main: { temp, feels_like, pressure, humidity },
+      visibility,
+      timezone,
+    } = currentWeather;
+
+    const [{ description, icon }] = weather;
+
+    const card = document.createElement("div");
+    card.classList.add("card", "card-lg", "current-weather-card");
+
+    card.innerHTML = `
+      <h2 class="title-2 card-title">Now</h2>
+
+      <div class="wrapper">
+        <p class="heading">${parseInt(temp)}&deg; <sup>c</sup></p>
+
+        <img
+          src="./assets/images/weather_icons/${icon}.png"
+          width="64"
+          height="64"
+          alt="${description}"
+          class="weather-icon"
+        />
+      </div>
+
+      <p class="body-3">${description}</p>
+
+      <ul class="meta-list">
+        <li class="meta-item">
+          <span class="m-icon">calendar_today</span>
+
+          <p class="title-3 meta-text">${module.getDate(dateUnix, timezone)}</p>
+        </li>
+
+        <li class="meta-item">
+          <span class="m-icon">location_on</span>
+
+          <p class="title-3 meta-text" data-location></p>
+        </li>
+      </ul>
+    
+    `;
+
+    fetchData(url.reverseGeo(lat, lon), function ([{ name, country }]) {
+      card.querySelector("[data-location]").textContent = `${name}, ${country}`;
+    });
+    currentWeatherSection.appendChild(card);
+
+    // TODAYS HIGHLIGHT
+
+    fetchData(url.airPollution(lat, lon), function (airPollution) {
+      const [
+        {
+          main: { aqi },
+          components: { no2, o3, so2, pm2_5 },
+        },
+      ] = airPollution.list;
+
+      const card = document.createElement("div");
+      card.classList.add("card", "card-lg");
+
+      const humidityStyle = function () {
+        if (humidity > 80) {
+          return "solid-red";
+        } else {
+          return "card-sm";
+        }
+      };
+
+      card.innerHTML = `
+        <h2 class="title-2" id="highlights-label">Today Highlights</h2>
+
+        <div class="highlight-list">
+          <div class="card card-sm highlight-card one">
+            <h3 class="title-3">Air Quality Index</h3>
+
+            <div class="wrapper">
+              <span class="m-icon">air</span>
+
+              <ul class="card-list">
+                <li class="card-item">
+                  <p class="title-1">${pm2_5.toPrecision(3)}</p>
+
+                  <p class="label-1">PM<sub>2.5</sub></p>
+                </li>
+
+                <li class="card-item">
+                  <p class="title-1">${so2.toPrecision(3)}</p>
+
+                  <p class="label-1">SO<sub>2</sub></p>
+                </li>
+
+                <li class="card-item">
+                  <p class="title-1">${no2.toPrecision(3)}</p>
+
+                  <p class="label-1">NO<sub>2</sub></p>
+                </li>
+
+                <li class="card-item">
+                  <p class="title-1">${o3}</p>
+
+                  <p class="label-1">O<sub>3</sub></p>
+                </li>
+              </ul>
+            </div>
+
+            <div>
+              <span class="badge aqi-${aqi} label-${aqi}" title="${module.aqiText[aqi].message}">
+                ${module.aqiText[aqi].level}
+              </span>
+            </div>
+          </div>
+
+          <div class="card card-sm highlight-card two glass-card">
+            <h3 class="title-3">Sunrise & Sunset</h3>
+
+            <div class="card-list">
+              <!-- Card Item 1 -->
+              <div class="card-item">
+                <span class="m-icon">clear_day</span>
+
+                <div>
+                  <p class="label-1">Sunrise</p>
+
+                  <p class="title-1">${module.getTime(sunriseUnixUTC, timezone)}</p>
+                </div>
+              </div>
+
+              <!-- Card Item 2 -->
+              <div class="card-item">
+                <span class="m-icon">clear_night</span>
+
+                <div>
+                  <p class="label-1">Sunset</p>
+
+                  <p class="title-1">${module.getTime(sunsetUnixUTC, timezone)}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- HUMIDITY -->
+          <div class="card ${humidityStyle()} highlight-card">
+            <h3 class="title-3">Humidity</h3>
+
+            <div class="wrapper">
+              <span class="m-icon">humidity_percentage</span>
+
+              <p class="title-1">${humidity}<sup>%</sup></p>
+            </div>
+          </div>
+
+          <!-- PRESSURE -->
+          <div class="card card-sm highlight-card">
+            <h3 class="title-3">Pressure</h3>
+
+            <div class="wrapper">
+              <span class="m-icon">airwave</span>
+
+              <p class="title-1">${pressure}<sub>hPa</sub></p>
+            </div>
+          </div>
+
+          <!-- VISIBILITY -->
+          <div class="card card-sm highlight-card">
+            <h3 class="title-3">Visibility</h3>
+
+            <div class="wrapper">
+              <span class="m-icon">visibility</span>
+
+              <p class="title-1">${visibility / 1000}<sup>km</sup></p>
+            </div>
+          </div>
+
+          <!-- FEELS LIKE -->
+          <div class="card solid-blue  highlight-card">
+            <h3 class="title-3">Feels Like</h3>
+
+            <div class="wrapper">
+              <span class="m-icon">thermostat</span>
+
+              <p class="title-1">${parseInt(feels_like)}&deg;<sup>C</sup></p>
+            </div>
+          </div>
+        </div>
+      `;
+
+      highlightSection.appendChild(card);
+    });
+
+    // 24H FORECAST SECTION
+
+    fetchData(url.forecast(lat, lon), function (forecast) {
+      const {
+        list: forecastList,
+        city: { timezone },
+      } = forecast;
+
+      hourlySection.innerHTML = `
+        <h2 class="title-2">Today at</h2>
+
+        <div class="slider-container">
+          <ul class="slider-list" data-temp></ul>
+          <ul class="slider-list" data-wind></ul>
+        </div>
+      
+      `;
+
+      for (const [index, data] of forecastList.entries()) {
+        if (index > 7) break;
+
+        const {
+          dt: dateTimeUnix,
+          main: { temp },
+          weather,
+          wind: { deg: windDirection, speed: windSpeed },
+        } = data;
+
+        const [{ icon, description }] = weather;
+
+        const tempLi = document.createElement("li");
+
+        tempLi.classList.add("slider-item");
+
+        tempLi.innerHTML = `
+          <div class="card card-sm slider-card">
+            <p class="body-3">${module.getHours(dateTimeUnix, timezone)}</p>
+            <img
+              src="./assets/images/weather_icons/${icon}.png"
+              width="48"
+              height="48"
+              loading="lazy"
+              alt="${description}"
+              class="weather-icon"
+              title="${description}"
+            />
+
+            <p class="body-3">${parseInt(temp)}&deg;</p>
+          </div>
+        `;
+
+        hourlySection.querySelector("[data-temp]").appendChild(tempLi);
+
+        const windLi = document.createElement("li");
+
+        windLi.classList.add("slider-item");
+
+        windLi.innerHTML = `
+          <div class="card card-sm slider-card">
+            <p class="body-3">${module.getHours(dateTimeUnix, timezone)}</p>
+            <img
+              src="./assets/images/weather_icons/direction.png"
+              width="48"
+              height="48"
+              loading="lazy"
+              alt="direction"
+              class="weather-icon"
+              title="${windDirection} degrees"
+              style = "transform: rotate(${windDirection - 180}deg)"
+            />
+
+            <p class="body-3">${parseInt(module.mps_to_kmh(windSpeed))} km/h</p>
+          </div>
+        `;
+
+        hourlySection.querySelector("[data-wind]").appendChild(windLi);
+      }
+
+      // 5DAY FORECAST SECTION
+
+      forecastSection.innerHTML = `
+        <h2 class="title-2" id="forecast-label">5 Day Forecast</h2>
+
+        <div class="card card-lg forecast-card">
+          <ul data-forecast-list></ul>
+        </div>
+      `;
+
+      for (let i = 7, len = forecastList.length; i < len; i += 8) {
+        const {
+          main: { temp_max },
+          weather,
+          dt_txt,
+        } = forecastList[i];
+
+        const [{ icon, description }] = weather;
+        const date = new Date(dt_txt);
+
+        const li = document.createElement("li");
+        li.classList.add("card-item");
+
+        li.innerHTML = `
+          <div class="icon-wrapper">
+            <img
+              src="./assets/images/weather_icons/${icon}.png"
+              width="36"
+              height="36"
+              alt="${description}"
+              class="weather-icon"
+              title="${description}"
+            />
+
+            <div class="span">
+              <p class="title-2">${parseInt(temp_max)}&deg;</p>
+            </div>
+          </div>
+
+          <p class="label-1">${date.getDate()} ${module.monthNames[date.getUTCMonth()]}</p>
+
+          <p class="label-1">${module.weekDayNames[date.getUTCDay()]}</p>
+        `;
+
+        forecastSection.querySelector("[data-forecast-list]").appendChild(li);
+      }
+
+      loading.style.display = "none";
+      // container.style.overflowY = "overlay";
+      container.classList.add("fade-in");
+    });
+  });
+};
+
+//MAP FUNCTION
+
+const map = L.map("map", { zoomControl: false }).setView([20, 78], 4); // India center
+
+// Dark mode tiles
+L.tileLayer("https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png", {
+  attribution: "&copy; OpenStreetMap &copy; CARTO",
+  maxZoom: 19,
+}).addTo(map);
+
+let previousMarker = null; // store the previous marker
+
+var locatorIcon = L.icon({
+  iconUrl: "./assets/images/locator.png",
+  iconSize: [32, 32],
+});
+
+L.icon.Default;
+
+map.removeControl(map.attributionControl);
+
+map.on("click", function (e) {
+  const { lat, lng } = e.latlng;
+
+  // Update weather
+  updateWeather(`lat=${lat}&lon=${lng}`);
+
+  // Remove previous marker if exists
+  if (previousMarker) {
+    map.removeLayer(previousMarker);
+  }
+
+  // Add new marker
+  const marker = L.marker([lat, lng], { icon: locatorIcon }).addTo(map);
+
+  // Optional: remove marker when clicked
+  marker.on("click", () => map.removeLayer(marker));
+
+  // Save this marker as previous
+  previousMarker = marker;
+});
+
+export const error404 = function () {
+  errorContent.style.display = "flex";
+};
