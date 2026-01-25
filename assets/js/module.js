951b354377487cef2df2a101700b96813a268fb5
sanathkkss12@gmail.com <sanathkkss12@gmail.com>
2026-01-25 11:43:46 +0530

    Bugs fixed

--- assets/js/module.js
+++ assets/js/module.js
@@ -1,6 +1,109 @@
+export const weekDayNames = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+export const monthNames = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
 /**
- * @license MIT
- * @fileoverview All module functions
- * @copyright codewithsadee 2023 All rights reserved
- * @author codewithsadee <mohammadsadee24@gmail.com>
+ *
+ * @param {number} dateUnix Unix date in sec
+ * @param {number} timezone Timezone shift from UTC in seconds
+ * @returns {string} Data String. formate: "Sunday 10, Jan"
  */
+export const getDate = function (dateUnix, timezone) {
+  const date = new Date((dateUnix + timezone) * 1000);
+  const weekDayName = weekDayNames[date.getUTCDay()];
+  const monthName = monthNames[date.getUTCMonth()];
+
+  return `${weekDayName} ${date.getUTCDate()}, ${monthName}`;
+};
+
+/**
+ *
+ * @param {number} timeUnix Unix time in sec
+ * @param {number} timezone Timezone shift from UTC in seconds
+ * @returns {string} Time String.
+ */
+export const getTime = function (timeUnix, timezone) {
+  const date = new Date((timeUnix + timezone) * 1000);
+  const hours = date.getUTCHours();
+  const minutes = date.getUTCMinutes();
+  const period = hours >= 12 ? "PM" : "AM";
+
+  return `${hours % 12 || 12}:${minutes} ${period}`;
+};
+
+/**
+ *
+ * @param {number} timeUnix Unix time in sec
+ * @param {number} timezone Timezone shift from UTC in seconds
+ * @returns {string} Time String.formate: "12:00 AM"
+ */
+export const getHours = function (timeUnix, timezone) {
+  const date = new Date((timeUnix + timezone) * 1000);
+  const hours = date.getUTCHours();
+  const period = hours >= 12 ? "PM" : "AM"; // Cheks if hours are greater than 12
+
+  return `${hours % 12 || 12} ${period}`;
+};
+
+/**
+ *
+ * @param {number} mps meters per second
+ * @returns {number} km/h
+ */
+export const mps_to_kmh = (mps) => {
+  const mph = mps * 3600;
+  return mph / 1000;
+};
+
+export const aqiText = {
+  1: {
+    level: "Good",
+    message:
+      "Air quality is considered satisfactory, and air pollution poses little or no risk.",
+  },
+
+  2: {
+    level: "Fair",
+    message:
+      "Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution.",
+  },
+
+  3: {
+    level: "Moderate",
+    message:
+      "Members of sensitive groups may experience health effects. The general public is not likely to be affected.",
+  },
+
+  4: {
+    level: "Poor",
+    message:
+      "Health alert: The risk of health effects is increased for everyone.",
+  },
+
+  5: {
+    level: "Very Poor",
+    message:
+      "Health warnings of emergency conditions. The entire population is more likely to be affected.",
+  },
+};
